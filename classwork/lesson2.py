399f364ae02f8f1965a123759815a04fb7ebc249
Diana\ Moroz <diva14@yandex.by>
2023-03-16 14:03:19 +0300

    homework02 task01

--- classwork/lesson2.py
+++ classwork/lesson2.py
@@ -1,3 +1,5 @@
 import main
 bernuli = main.combination(2000, 3) * (0.0015**3) * (0.9985**1997)
 print(bernuli)
+ber = main.combination(5, 2) * 0.9**2 * 0.1**3
+print(ber)
--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import math
 
-def combination(n:int, k:int):
-    return math.factorial(n)/(math.factorial(k)*math.factorial(n-k))
 
-def permutation(n:int):
+def combination(n: int, k: int):
+    return math.factorial(n) / (math.factorial(k) * math.factorial(n - k))
+
+
+def permutation(n: int):
     return math.factorial(n)
 
-def accomadation(n:int, k:int):
-    return math.factorial(n)/math.factorial(n-k)
+
+def accomadation(n: int, k: int):
+    return math.factorial(n) / math.factorial(n - k)
+
+
+def bernulli(n: int, k: int, p: float):
+    q = 1 - p
+    return combination(n, k) * p ** k * q ** (n - k)
+
+def puasson(n, p, m):
+    la = p*n
+    return (la**m / math.factorial(m)) * math.exp(-la)
+
