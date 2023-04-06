29c84bb74dfd401f18f89099c9a16d931a9da898
Diana\ Moroz <diva14@yandex.by>
2023-04-06 13:27:55 +0300

    task03 homework08

--- homework08/task02.py
+++ homework08/task02.py
@@ -8,10 +8,11 @@ from scipy import stats
 import numpy as np
 
 student_iq = np.array([131, 125, 115, 122, 131, 115, 107, 99, 125, 111])
-z = stats.norm.ppf(0.975)
+
 std_g = np.std(student_iq)
 mean_x = np.mean(student_iq)
 n = len(student_iq)
-left = mean_x - z*(std_g/np.sqrt(n))
-right = mean_x + z*(std_g/np.sqrt(n))
+t = stats.t.ppf(0.975, n-1)
+left = mean_x - t*(std_g/np.sqrt(n))
+right = mean_x + t*(std_g/np.sqrt(n))
 print(f'Интервал математического ожидания с надежностью 0,95 составляет ({round(left, 3)}, {round(right, 3)})')
